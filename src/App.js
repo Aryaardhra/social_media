c92e0fecb55bc254f9d69736a9d7a2cba11d473e
Aryaardhra <ardhra455@gmail.com>
2023-06-03 18:26:35 +0530

    facebook login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { LoginSocialFacebook } from 'reactjs-social-login';
+import { FacebookLoginButton } from 'react-social-login-buttons';
 
-function App() {
+export default function App() {
+
+  const [profile, setProfile] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+    <LoginSocialFacebook
+     appId='1262911381284349'
+     onResolve={(response) => {
+     console.log(response);
+     setProfile(response.data);
+    }}
+    onReject={(error)=>{
+    console.log(error);
+    }}
+   >
+   <FacebookLoginButton/>
+   </LoginSocialFacebook>
+
+   {profile? <span>{profile.name}</span> :" "}
+   </>
+  )
 }
 
-export default App;
